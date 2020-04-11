e50085cfed7995ef3c2ec3e5c8d4fe25790ba5bb
Erin Beachkofski <beachko2@uwm.edu>
2020-04-10 21:27:11 -0500

    Blank input check

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ var UIController = (function() {
 			return {
 				type: document.querySelector(DOMstrings.inputType).value,		//will be either inc or exp
 				description: document.querySelector(DOMstrings.inputDescription).value,
-				value: document.querySelector(DOMstrings.inputValue).value
+				value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
 			}	
 		},
 
@@ -144,25 +144,36 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 	var DOM = UICtrl.getDOMstrings();
 
+	var updateBudget = function(){
+		// 1. Calculate the budget 
+
+		// 2. Return the budget
+
+		// 3. Display the budget on the UI
+
+	}
+
 	var ctrlAddItem = function() {
 		var input, newItem;
 
 		// 1. Get the field input data
 		input = UICtrl.getInput();
 
-		// 2. Add the item to the budget controller
-		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+		if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+			// 2. Add the item to the budget controller
+			newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-		// 3a. Add the item to the UI
-		UICtrl.addListItem(newItem, input.type);
+			// 3a. Add the item to the UI
+			UICtrl.addListItem(newItem, input.type);
 
-		// 3b. Clear the fields
-		UICtrl.clearFields();
+			// 3b. Clear the fields
+			UICtrl.clearFields();
 
-		// 4. Calculate the budget 
+			// 4. Calculate and update budget
+			updateBudget();
+		}
+
 		
-		// 5. Display the budget on the UI
-
 	}
 
 	return {
